9158cc433fe2956c8fcda04f5317c1b9209636e8
MrHope312 <shrey@hope.com>
2019-01-14 19:19:10 +0530

    updates

--- pages/illustration/index.vue
+++ pages/illustration/index.vue
@@ -2,15 +2,7 @@
   <div class="home-page">
     <div class="bg">
       <Header />
-<<<<<<< HEAD
-      <NavBar id="holla" />
-=======
-<<<<<<< HEAD
-      <NavBar/>
-=======
       <NavBar />
->>>>>>> 74bd714461ca9465de4847bc650943bfea0db75d
->>>>>>> 1a47b4a6df53d5b6b7daeecff3ffbe2fa927a696
     </div>
   </div>
 </template>
