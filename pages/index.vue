1d813692e800145d3b9d59954180e2abb4bfba6a
MrHope312 <shrey@hope.com>
2019-01-25 19:43:38 +0530

    updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,88 @@
 <template>
-  <div class="hp" >
+  <div class="hp">
     <div class="scrolldiv" v-scroll="handlescroll">
-    <div class="logo"><nuxt-link to='/'>
-      <img src="~/assets/img/logo.png">
-    </nuxt-link></div>
-    <nav class="navbar">
+      <div class="logo">
+        <nuxt-link to='/'>
+          <img src="~/assets/img/logo.png">
+        </nuxt-link>
+      </div>
+      <nav class="navbar">
         <nuxt-link class="items" to='/services'>SERVICES</nuxt-link>
         <nuxt-link class="items" to='/projects'>PROJECTS</nuxt-link>
         <nuxt-link class="items" to='/aboutus'>ABOUT US</nuxt-link>
         <nuxt-link class="items" to='/contactus'>CONTACT US</nuxt-link>
-    </nav>
-    <div class="bg molla" >
-      <div class="content-left">
-        <img src="~/assets/img/hastha.png" width="30%" height="30%">
+      </nav>
+      <div class="toggle-btn" @click="togglebtn">
+        <span class="one"></span>
+        <span class="two"></span>
+      </div>
+      <div class="menu" style="display:none">
+        <div class="data">
+          <ul>
+            <li>
+              <nuxt-link class="hitems" to='/services'>SERVICES</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link class="hitems" to='/projects'>PROJECTS</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link class="hitems" to='/aboutus'>ABOUT US</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link class="hitems" to='/contactus'>CONTACT US</nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="bg molla">
+        <div class="content-left">
+          <img src="~/assets/img/hastha.png" width="30%" height="30%">
           <div class="second">
             <p>A Revolutionary</p>
             <p>Company</p>
           </div>
+        </div>
+        <div class="content-right">
+          <h2>
+            <button id='gs1' @click="onclick">KNOW MORE</button>
+          </h2>
+        </div>
       </div>
-      <div class="content-right">
+      <div class="bg holla">
+        <div class="container">
+          <div class="item1">
+            <h1 class="heading h_vis">VISION</h1>
+            <section class="intro content i_vis">
+              <div class="vision">
+                To provide training and internship for minimum 200 students every year and to create an
+                opportunity to work on live projects.
+              </div>
+              <div class="vision">To create opportunities for other disciplines having interdependence on real estate
+                industry.</div>
+              <div class="vision"> To prepare and provide code of AI in all domains of real estate industry.</div>
+              <div class="vision">To prepare and write general guidelines for real estate industries which should
+                streamline the
+                unorganized sector</div>
+            </section>
+          </div>
+          <div class="item2">
+            <h1 class="heading h_strat">OBJECTIVE</h1>
+            <section class="intro content i_strat">
+              <div class="strat">Creating vertical financial growth and horizontal spread across the country.</div>
+              <div class="strat">To create a common platform for architects, Planners, Engineers, Interior designers,
+                Developers, and also
+                for immovable property valuer, dealers, consultants, legal advisers and execute turnkey project
+                execution.<br></div>
+              <div class="strat">To integrate the technical education with real estate industry.</div>
+              <div class="strat">To incubate the innovative startups on real estate platform.</div>
+              <div class="strat"> Eventually, turn the company under the public holding format.</div>
+            </section>
+          </div>
+        </div>
         <h2>
-          <button id='gs1' @click="onclick">KNOW MORE</button>
+          <button id='gs1' @click="onclick">ACCOLADES</button>
         </h2>
       </div>
-    </div>
-    <div class="bg holla" >
-      <div class="container">
-        <div class="item1">
-          <h1 class="heading h_vis">VISION</h1>
-          <section class="intro content i_vis">
-            <div class="vision">
-              To provide training and internship for minimum 200 students every year and to create an
-              opportunity to work on live projects.
-            </div>
-            <div class="vision">To create opportunities for other disciplines having interdependence on real estate
-              industry.</div>
-            <div class="vision"> To prepare and provide code of AI in all domains of real estate industry.</div>
-            <div class="vision">To prepare and write general guidelines for real estate industries which should
-              streamline the
-              unorganized sector</div>
-          </section>
-        </div>
-        <div class="item2">
-          <h1 class="heading h_strat">OBJECTIVE</h1>
-          <section class="intro content i_strat">
-            <div class="strat">Creating vertical financial growth and horizontal spread across the country.</div>
-            <div class="strat">To create a common platform for architects, Planners, Engineers, Interior designers,
-              Developers, and also
-              for immovable property valuer, dealers, consultants, legal advisers and execute turnkey project
-              execution.<br></div>
-            <div class="strat">To integrate the technical education with real estate industry.</div>
-            <div class="strat">To incubate the innovative startups on real estate platform.</div>
-            <div class="strat"> Eventually, turn the company under the public holding format.</div>
-          </section>
-        </div>
-      </div>
-      <h2>
-        <button id='gs1' @click="onclick">ACCOLADES</button>
-      </h2>
-    </div>
     </div>
   </div>
 </template>
@@ -70,8 +94,14 @@
   } from 'gsap'
 
   export default {
-    methods:{
-    onclick() {
+    data() {
+      return {
+        sw: false,
+        sw2: false
+      }
+    },
+    methods: {
+      onclick() {
         const tl = new TimelineMax()
         tl.add('enter')
         tl.fromTo('.molla', 1, {
@@ -81,41 +111,84 @@
           delay: 2,
           transform: "translate(-200%)"
         }, 'enter')
-  },
-  handlescroll(evt,el){
-    if(window.scrollY > 1){
-        const tl = new TimelineMax()
-        tl.add('enter')
-        tl.fromTo('.molla', 1, {
-          opacity: 1
-        }, {
-          delay: 1,
-          transform: "translate(-200%)"
-        }, 'enter')
-    }
-    if(window.scrollY < 1){
-        const tl = new TimelineMax()
-        tl.add('enter')
-        tl.fromTo('.molla', 1, {
-          opacity: 1
-        }, {
-          delay: 1,
-          transform: "translate(0)"
-        }, 'enter')
+      },
+      handlescroll(evt, el) {
+        if (window.scrollY > 10) {
+          const tl = new TimelineMax()
+          tl.add('enter')
+          
+          tl.fromTo('.molla', 1, {
+            opacity: 1,
+            delay: -1
+          }, {
+            transform: "translate(-130%)"
+          }, 'enter')
+
+          tl.to('.toggle-btn', 0.5,{
+            display: "block",
+            delay: -1
+          })
+          tl.to('.navbar', 0.5,{
+            display:"none",
+            delay: -1,
+            opacity: 0
+          })
+        }
+        if (window.scrollY < 1) {
+          const tl = new TimelineMax()
+          tl.add('enter')
+          tl.fromTo('.molla', 1, {
+            opacity: 1,
+            delay:-1
+          }, {
+            transform: "translate(0)"
+          }, 'enter')
+
+          tl.to('.toggle-btn', 0.05,{
+            display: "none",
+            delay: -1
+          })
+          tl.to('.navbar', 0.5,{
+            display:"flex",
+            delay: -1,
+            opacity:1
+          })
+        }
+      },
+      togglebtn() {
+        let tl2 = new TimelineMax()
+        if (this.sw == false) {
+          tl2.add('enter')
+          tl2.to(".menu", 1, {
+            display: "block",
+            top: "0%",
+            ease: Expo.easeInOut,
+            delay:-1
+          })
+          this.sw=true
+        }
+        else{
+          tl2.add('enter')
+          tl2.to(".menu", 1,{
+            display:"none",
+            delay:-1
+          })
+          this.sw=false
+        }
+      }
     }
   }
-}
-}
 </script>
 <style scoped>
   .hp {
     font-weight: 100;
     font-family: 'Quicksand', sans-serif;
   }
-  .scrolldiv{
+
+  .scrolldiv {
     height: 1080px;
-    overflow: visible;
   }
+
   .logo {
     padding: 0;
     margin: 0;
@@ -124,29 +197,93 @@
     z-index: 100;
     top: 10%;
     left: 5%;
-    position:fixed;
+    position: fixed;
   }
-  .navbar{
+
+  .navbar {
     box-sizing: border-box;
-    display:flex;
+    display: flex;
     justify-content: center;
     position: absolute;
-    top:3%;
+    top: 3%;
     right: 3%;
-    z-index:100;
-    position:fixed;
+    z-index: 100;
+    position: fixed;
   }
+
   .items {
-        text-decoration:none;
-        color:rgb(22, 22, 22);
-        font-family: 'Quicksand', sans-serif;
-        font-weight:100;
-        font-size:1.5em;
-        padding: 2em;
+    text-decoration: none;
+    color: rgb(56, 56, 56);
+    font-family: 'Quicksand', sans-serif;
+    font-weight: 100;
+    font-size: 1.5em;
+    padding: 2em;
+  }
+
+  .hitems {
+    text-decoration: none;
+    color: rgb(255, 255, 255);
+    font-family: 'Quicksand', sans-serif;
+    font-weight: 100;
+    font-size: 1.5em;
+    padding: 2em;
+  }
+
+  .toggle-btn {
+    position: absolute;
+    top:5%;
+    left:90%;
+    margin: 1.4em;
+    padding: 1em;
+    width: 40px;
+    z-index: 101;
+    right: 0;
+    cursor: pointer;
+    position: fixed;
+    display:none;
+  }
+
+  span.one {
+    position: absolute;
+    width: 40px;
+    height: 2px;
+    background: rgb(80, 79, 79);
+  }
+
+  span.two {
+    position: absolute;
+    width: 40px;
+    height: 2px;
+    background: rgb(80, 79, 79);
+    margin-top: 6px;
+  }
+
+  .menu {
+    z-index: 100;
+    position: absolute;
+    background: #5c5c5c;
+    height: 91vh;
+    width: 95.5vw;
+    padding: 2em;
+    margin: 3em;
+    position: fixed;
+  }
+
+  .data {
+    padding: 8em 0 0 2em;
+    text-align: left;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  li {
+    font-size: 42px;
   }
 
   .bg {
-    height: 93vh;
+    height: 91vh;
     width: 95.5vw;
     margin: 3em;
     padding: 2em;
@@ -156,15 +293,19 @@
     background-repeat: no-repeat;
     background-size: cover;
     box-shadow: 0 10px 10px 3px #535353b6;
-    position:absolute;
-    z-index:1;
+    position: absolute;
+    z-index: 1;
 
   }
-  .holla{
+
+  .holla {
     position: fixed;
-    z-index:0;
+    z-index: 0;
+    background-image: linear-gradient(to right bottom, rgba(255, 255, 255, 0.657), rgba(63, 63, 63, 0)), url('~assets/img/bg2.jpg');
+    background-position: center;
   }
-  .molla{
+
+  .molla {
     position: fixed;
   }
 
@@ -176,12 +317,14 @@
     font-family: 'Quicksand', sans-serif;
     width: 500px;
   }
+
   .content-right {
     position: absolute;
 
     top: 85%;
     left: 82%;
   }
+
   .second {
     position: absolute;
     font-family: 'Quicksand', sans-serif;
@@ -241,11 +384,11 @@
 
   #gs1 {
     background-color: Transparent;
-    background-repeat:no-repeat;
+    background-repeat: no-repeat;
     border: none;
-    cursor:pointer;
+    cursor: pointer;
     overflow: hidden;
-    outline:none;
+    outline: none;
     position: absolute;
     width: 200px;
     bottom: 0%;
